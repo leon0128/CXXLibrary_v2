77d9a573d1f134607ff5417e22472d3fe35813bf
leon0128 <18aj099@ms.dendai.ac.jp>
2020-06-09 16:47:00 +0900

    [update] implements cstddef.hpp

--- src/cstddef.hpp
+++ src/cstddef.hpp
@@ -24,33 +24,69 @@ enum class byte : unsigned char {};
 // byte type operations
 // operator <<=
 template<class IntType>
-constexpr byte& operator<<=(byte& b, IntType shift) noexcept;
+constexpr byte& operator<<=(byte& b, IntType shift) noexcept
+{
+    return b = byte(static_cast<unsigned char>(b) << shift);
+}
 // operator <<
 template<class IntType>
-constexpr byte operator<<(byte b, IntType shift) noexcept;
+constexpr byte operator<<(byte b, IntType shift) noexcept
+{
+    return byte(static_cast<unsigned char>(b) << shift);
+}
 // operator >>=
 template<class IntType>
-constexpr byte& operator>>=(byte& b, IntType shift) noexcept;
+constexpr byte& operator>>=(byte& b, IntType shift) noexcept
+{
+    return b = byte(static_cast<unsigned char>(b) >> shift);
+}
 // operator >>
 template<class IntType>
-constexpr byte operator>>(byte b, IntType shift) noexcept;
+constexpr byte operator>>(byte b, IntType shift) noexcept
+{
+    return byte(static_cast<unsigned char>(b) >> shift);
+}
 // operator |=
-constexpr byte& operator|=(byte& l, byte r) noexcept;
+constexpr byte& operator|=(byte& l, byte r) noexcept
+{
+    return l = byte(static_cast<unsigned char>(l) | static_cast<unsigned char>(r));
+}
 // operator |
-constexpr byte operator|(byte l, byte r) noexcept;
+constexpr byte operator|(byte l, byte r) noexcept
+{
+    return byte(static_cast<unsigned char>(l) | static_cast<unsigned char>(r));
+}
 // operator &=
-constexpr byte& operator&=(byte& l, byte r) noexcept;
+constexpr byte& operator&=(byte& l, byte r) noexcept
+{
+    return l = byte(static_cast<unsigned char>(l) & static_cast<unsigned char>(r));
+}
 // operator &
-constexpr byte operator&(byte l, byte r) noexcept;
+constexpr byte operator&(byte l, byte r) noexcept
+{
+    return byte(static_cast<unsigned char>(l) & static_cast<unsigned char>(r));
+}
 // operator ^=
-constexpr byte& operator^=(byte& l, byte r) noexcept;
+constexpr byte& operator^=(byte& l, byte r) noexcept
+{
+    return l = byte(static_cast<unsigned char>(l) ^ static_cast<unsigned char>(r));
+}
 // operator ^
-constexpr byte operator^(byte l, byte r) noexcept;
+constexpr byte operator^(byte l, byte r) noexcept
+{
+    return byte(static_cast<unsigned char>(l) ^ static_cast<unsigned char>(r));
+}
 // operator ~
-constexpr byte operator~(byte b) noexcept;
+constexpr byte operator~(byte b) noexcept
+{
+    return byte(~static_cast<unsigned char>(b));
+}
 // to_integer
 template<class IntType>
-constexpr IntType to_integer(byte b) noexcept;
+constexpr IntType to_integer(byte b) noexcept
+{
+    return IntType(b);
+}
 
 }
 
